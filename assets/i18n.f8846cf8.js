18f27eeca9d8146013c7926dda65188b6ddff85b
thomasdeparis <th.deparis@gmail.com>
2024-03-23 15:55:47 +0000

    auto CI deploy from last commit/pr

--- assets/i18n.f8846cf8.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index.44d67d2d.js";import{i}from"./index.c068a55c.js";import"./vue-i18n.runtime.1ff69fdb.js";var p=r(({app:o})=>{o.use(i)});export{p as default};
---
+++ assets/i18n.ff2c7fed.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index.c32bac98.js";import{i}from"./index.b86b291e.js";import"./vue-i18n.runtime.27bfa109.js";var p=r(({app:o})=>{o.use(i)});export{p as default};
