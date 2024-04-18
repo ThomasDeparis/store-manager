1cb0a4dcf9d51abe15496944cc19f88354769187
thomasdeparis <th.deparis@gmail.com>
2024-04-18 19:12:20 +0000

    auto CI deploy from last commit/pr

--- assets/IndexPage.528d60a0.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.f473b792.js";import{_ as a,d as t,H as o,I as n,J as s,R as r}from"./index.5f0495ac.js";const c=t({name:"IndexPage"}),d=r("h2",null,"Page d'accueil",-1);function _(p,i,f,l,m,u){return o(),n(e,{padding:""},{default:s(()=>[d]),_:1})}var h=a(c,[["render",_]]);export{h as default};
